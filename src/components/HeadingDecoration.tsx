312d2933ec4b8f6b302c1d996c309b32aaed538a
Jinho Yim <jinhoyim@outlook.com>
2024-10-31 21:28:35 +0900

    헤딩 배경에 꾸밈 요소 추가

---
+++ src/components/HeadingDecoration.tsx
@@ -0,0 +1,7 @@
+import styles from './HeadingDecoration.module.css';
+
+export default function HeadingDecoration(): JSX.Element {
+    return (
+        <div className={styles.wave}></div>
+    );
+}
