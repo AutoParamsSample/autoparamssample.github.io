8207f34566d9df2ffc6ec03023b34e3127c8ce8f
Jinho Yim <jinhoyim@outlook.com>
2024-11-02 05:00:18 +0900

    SEO 메타 정보 수정 및 첫 페이지 og:description 적용을 위해 Layout description 속성 할당

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,7 +47,7 @@ const config: Config = {
   themeConfig: {
     metadata: [
       { name: 'keywords', content: 'AutoParams, Java testing, test data generation, parameterized tests, JUnit, AutoFixture, automated testing, open-source testing tools, random test data, Java unit tests' },
-      { name: 'description', content: 'AutoParams simplifies Java and Kotlin testing by generating arbitrary data for JUnit5 parameterized tests, inspired by AutoFixture. Automate test data creation with minimal setup and maintain focus on testing logic.' },
+      { name: 'description', content: 'AutoParams is a versatile test data generator designed for parameterized tests in Java and Kotlin, drawing inspiration from AutoFixture.' },
     ],
     navbar: {
       title: 'AutoParams',
